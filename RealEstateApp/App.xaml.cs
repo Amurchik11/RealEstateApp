c23f4c0f1b419ead782784e3303eb1301cf17161
dolma <dolma@ROMASHKA>
2023-01-07 11:19:42 +0200

    Добавьте файлы проекта.

---
+++ RealEstateApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RealEstateApp.Pages;
+
+namespace RealEstateApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+       
+            MainPage = new RegisterPage();
+        
+    }
+}
---
+++ RealEstateApp/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl = "http://amurchichek-001-site1.ftempurl.com/";
+         
+    }
+}
